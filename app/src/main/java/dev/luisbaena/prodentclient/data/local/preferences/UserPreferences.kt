7979de20a6db40ef7bfe6317017ecce382d2d7a0
lbaeutr <lbaeutr@gmail.com>
2025-09-28 20:19:50 +0200

    UsuarioPreferences configuracion

--- app/src/main/java/dev/luisbaena/prodentclient/data/local/preferences/UserPreferences.kt
+++ app/src/main/java/dev/luisbaena/prodentclient/data/local/preferences/UserPreferences.kt
@@ -4,20 +4,128 @@ import android.content.Context
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.booleanPreferencesKey
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import javax.inject.Singleton
+import dev.luisbaena.prodentclient.domain.model.User
+
+
+
+/*
+ * En este archivo se maneja el almacenamiento local de las preferencias del usuario
+    * utilizando DataStore de Android.
+    * Defino diferentes  funciones para guardar, obtener y borrar las preferencias del usuario.
+    * Esta clase es inyectada  en otras partes de la aplicación usando Hilt.
+    * El archivo de preferencias se llama "prodent_user_prefs".
+    * Las funciones que encontramos son:
+        - saveUser: Guarda los datos del usuario en las preferencias.
+        - getUser: Obtiene los datos del usuario de las preferencias.
+        - clearUser: Borra todas las preferencias del usuario.
+        - isLoggedIn: Verifica si el usuario está logueado.
+        - getUserToken: Obtiene el token de autenticación del usuario.
+        - getUserFlow: Proporciona un flujo reactivo de los datos del usuario.
+    * Las claves usadas para almacenar los datos son definidas como constantes en el companion object.
+
+ */
+
 
 val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "prodent_user_prefs")
 
 @Singleton
-class UserPreferences @Inject constructor(private val context: Context) {
+class UserPreferences @Inject constructor(@ApplicationContext private val context: Context) {
 
     companion object {
-       //TODO terminar
+        private val KEY_USER_ID = stringPreferencesKey("user_id")
+        private val KEY_USER_NAME = stringPreferencesKey("user_name")
+        private val KEY_USER_LASTNAME = stringPreferencesKey("user_lastname")
+        private val KEY_USER_EMAIL = stringPreferencesKey("user_email")
+        private val KEY_USER_PHONE = stringPreferencesKey("user_phone")
+        private val KEY_USER_TOKEN = stringPreferencesKey("user_token")
+        private val KEY_USER_ROLE = stringPreferencesKey("user_role")
+        private val KEY_IS_LOGGED_IN = booleanPreferencesKey("is_logged_in")
+
     }
 
+    suspend fun saveUser(user: User) {
+        context.dataStore.edit { prefs ->
+            prefs[KEY_USER_ID] = user.id
+            prefs[KEY_USER_NAME] = user.nombre
+            prefs[KEY_USER_LASTNAME] = user.apellido
+            prefs[KEY_USER_EMAIL] = user.email
+            prefs[KEY_USER_PHONE] = user.telefono
+            prefs[KEY_USER_TOKEN] = user.token
+            prefs[KEY_USER_ROLE] = user.role
+            prefs[KEY_IS_LOGGED_IN] = true
+        }
+    }
 
+    suspend fun getUser(): User?{
+        val preferences = context.dataStore.data.first()
 
+        val isLoggedIn = preferences[KEY_IS_LOGGED_IN] ?: false
+        if (!isLoggedIn)  return null
 
+        val id = preferences[KEY_USER_ID] ?: return null
+        val email = preferences[KEY_USER_EMAIL] ?: return null
+        val phone = preferences[KEY_USER_PHONE] ?: return null
+        val name = preferences[KEY_USER_NAME] ?: return null
+        val lastname = preferences[KEY_USER_LASTNAME] ?: return null
+        val token = preferences[KEY_USER_TOKEN] ?: return null
+        val role = preferences[KEY_USER_ROLE] ?: return null
 
+        if (id.isBlank() || token.isBlank()) {
+            clearUser()
+            return null
+        }
 
+        return User(
+            id = id,
+            nombre = name,
+            apellido = lastname,
+            email = email,
+            telefono = phone,
+            token = token,
+            role = role
+        )
+    }
 
+    suspend fun clearUser() {
+        context.dataStore.edit { prefs ->
+            prefs.clear()
+        }
+    }
+
+    suspend fun isLoggedIn(): Boolean {
+        val preferences = context.dataStore.data.first()
+        return preferences[KEY_IS_LOGGED_IN] ?: false
+    }
+
+    suspend fun getUserToken(): String? {
+        val preferences = context.dataStore.data.first()
+        return if (preferences[KEY_IS_LOGGED_IN] == true) {
+            preferences[KEY_USER_TOKEN]
+        } else  null
+    }
+
+    fun getUserFlow() = context.dataStore.data.map { prefs ->
+        val isLoggedIn = prefs[KEY_IS_LOGGED_IN] ?: false
+        if (!isLoggedIn)  return@map null
+
+        val id = prefs[KEY_USER_ID] ?: return@map null
+        val email = prefs[KEY_USER_EMAIL] ?: return@map null
+        val phone = prefs[KEY_USER_PHONE] ?: return@map null
+        val name = prefs[KEY_USER_NAME] ?: return@map null
+        val lastname = prefs[KEY_USER_LASTNAME] ?: return@map null
+        val token = prefs[KEY_USER_TOKEN] ?: return@map null
+        val role = prefs[KEY_USER_ROLE] ?: return@map null
+
+        if (id.isBlank() || token.isBlank()) return@map null
+
+        User(id, name, lastname, email, phone, token, role)
+    }
 }
